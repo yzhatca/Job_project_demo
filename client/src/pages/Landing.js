85eace85aff4a9be3d2086e5fda0f43139edb2fe
unknown <yzharsenal@gmail.com>
2023-03-06 18:33:53 -0500

    full project setup

---
+++ client/src/pages/Landing.js
@@ -0,0 +1,41 @@
+import job_hunt from '../assets/images/job_hunt.svg';
+import Wrapper from '../assets/wrappers/LandingPage';
+import { Logo } from '../components';
+import { Link } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
+import { useAppContext } from '../context/appContext';
+import React from 'react';
+
+const Landing = () => {
+  const { user } = useAppContext();
+  return (
+    <React.Fragment>
+      {user && <Navigate to='/' />}
+      <Wrapper>
+        <nav>
+          <Logo />
+        </nav>
+        <div className='container page'>
+          {/* info */}
+          <div className='info'>
+            <h1>
+              job <span>hunting</span> app
+            </h1>
+            <p>
+              I'm baby wayfarers hoodie next level taiyaki brooklyn cliche blue
+              bottle single-origin coffee chia. Aesthetic post-ironic venmo,
+              quinoa lo-fi tote bag adaptogen everyday carry meggings +1 brunch
+              narwhal.
+            </p>
+            <Link to='/register' className='btn btn-hero'>
+              Login/Register
+            </Link>
+          </div>
+          <img src={job_hunt} alt='job hunt' className='img main-img' />
+        </div>
+      </Wrapper>
+    </React.Fragment>
+  );
+};
+
+export default Landing;
